c4e922f09eb687d655fd84d344a940f2a2f53c44
= <andrewboudreau@gmail.com>
2023-05-01 23:32:43 -0500

    Add project files.

---
+++ Pages/FallingRainbow.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace KennethWebDemos.Pages
+{
+    public class FallingRainbowModel : PageModel
+    {
+        private readonly ILogger<IndexModel> logger;
+
+        public FallingRainbowModel(ILogger<IndexModel> logger)
+        {
+            this.logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
